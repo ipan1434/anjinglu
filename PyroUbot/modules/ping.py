24152c989ae5244bb33c760025a5063549d78579
ipan1434 <flaelva6@gmail.com>
2025-05-02 00:21:40 +0700

    Update ping.py

--- PyroUbot/modules/ping.py
+++ PyroUbot/modules/ping.py
@@ -20,7 +20,7 @@ async def _(client, message):
     await client.invoke(Ping(ping_id=0))
     end = datetime.now()
     uptime = await get_time((time() - start_time))
-    delta_ping_formatted = round((end - start).microseconds / 00000, 0)
+    delta_ping_formatted = round((end - start).microseconds / 00000, 2)
     pong = await EMO.PING(client)
     tion = await EMO.MENTION(client)
     yubot = await EMO.UBOT(client)
@@ -33,13 +33,15 @@ async def _(client, message):
         _ping = f"""
 <blockquote>{pong} {pantek} : {str(delta_ping_formatted).replace('.', ',')} ms
 {tion} {ngentod} : <code>{client.me.mention}</code>
-{yubot} {kontol} : <code>{bot.me.mention}</code></blockquote>"""
+{yubot} {kontol} : <code>{bot.me.mention}</code></blockquote>
+"""
         await message.reply(_ping)
     else:
         _ping = f"""
 <blockquote>{pantek} : {str(delta_ping_formatted).replace('.', ',')} ms
 {ngentod} : <code>{client.me.mention}</code>
-{kontol} : <code>{bot.me.mention}</code></blockquote>"""
+{kontol} : <code>{bot.me.mention}</code></blockquote>
+"""
         await message.reply(_ping)
 
 @PY.INDRI("1ping")
